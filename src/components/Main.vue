ac899051a4f04c389277671d99ec1869b7fd9cd0
Yanluoluo <1799191542@qq.com>
2023-04-18 17:44:55 +0800

    feat:增加npc快捷选择功能

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -10,17 +10,35 @@
         <th>描述</th>
         <th>地点</th>
         <th>时间</th>
+        <th>快捷操作</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="(item, index) in date" :key="index">
-        <td>{{ item.person }}</td>
+        <!-- npc选择,单击进入编辑,双击快速选择 -->
+        <td @dblclick="item.person.isEditor = true">
+          <span v-show="!item.person.isEditor">{{ item.person.date }}</span>
+          <!-- 输入框 -->
+          <Input
+            v-show="item.person.isEditor"
+            :value="item.person.date"
+            @handle-end-input="
+              (value) => {
+                [item.person.date, item.person.isEditor] = [value, false];
+              }
+            "
+          />
+        </td>
+        <!-- 编辑框 -->
         <td>{{ item.description }}</td>
+        <!-- 地点框 -->
         <td>{{ item.location }}</td>
+        <!-- 时间轴 -->
         <td>{{ item.time }}</td>
+        <td>Del</td>
       </tr>
       <tr>
-        <td class="addButton" :colspan="4">添加</td>
+        <td class="addButton" :colspan="4" @click="addNewDescription">添加</td>
       </tr>
     </tbody>
     <tfoot></tfoot>
@@ -28,15 +46,37 @@
 </template>
 <script lang="ts" setup>
 import { reactive, ref } from "vue";
+import Input from "./Input.vue";
+import { useStore } from "../store";
+const store = useStore();
 
 // 剧本树
 const date = reactive([
   {
-    person: "破布袍神秘人",
+    person: { date: "破布袍神秘人", isEditor: false },
     description: "拿起了桌子上的烛台",
     location: "沉眠王座",
     time: "14:26",
   },
 ]);
+
+function addNewDescription() {
+  date.push({
+    person: {
+      date:
+        (store.state.personList[store.state.personCheckId] &&
+          store.state.personList[store.state.personCheckId].name) ||
+        "",
+      isEditor: false,
+    },
+    description: "",
+    location: date[date.length - 1].location,
+    time: "",
+  });
+}
+
+function test(item: any) {
+  console.log(item.person);
+}
 </script>
 <style lang="css"></style>
