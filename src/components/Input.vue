478dc4e5476c7d9da0f0d9ab939ec24f6c49c33c
Yanluoluo <1799191542@qq.com>
2023-04-22 14:50:37 +0800

    feat;调整输入框组件的type接收,现在有常规和圆角两种

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,37 +1,53 @@
 <template>
   <div :class="props.type">
     <span class="front" v-show="props.isDescription">{{ front }}</span>
-    <div class="input" ref="input" contenteditable="true" @blur="emit('handleEndInput', input.innerHTML)"
-      @keydown.enter="emit('handleEndInput', input.innerHTML)">
+    <div
+      class="input"
+      ref="input"
+      contenteditable="true"
+      :class="type"
+      @blur="emit('handleEndInput', input.innerHTML)"
+      @keydown.enter="emit('handleEndInput', input.innerHTML)"
+    >
       {{ value }}
     </div>
-    <!-- <span class="after">{{ after }}</span> -->
   </div>
 </template>
 <script lang="ts" setup>
 import { ref, onMounted, computed } from "vue";
-
-const props = defineProps(["type", "value", "isDescription"]);
+// type 的种类 "default","round"
+const props = defineProps({
+  // 输入框种类
+  type: {
+    type: String,
+    default: "default",
+    validator(value) {
+      const types = ["default", "round"];
+      return typeof value === "string" && types.includes(value);
+    },
+  },
+  // 具体值
+  value: {
+    type: String,
+    default: "输入框",
+  },
+  isDescription: {
+    type: Boolean,
+    default: false,
+  },
+});
 const emit = defineEmits(["handleEndInput"]);
 const input = ref();
 const value = ref("");
 
-
-
 const front = computed(() => {
-  if (props.type === "action") return "\["
-  else if (props.type === "narrator") return "\{"
-  else return ""
-})
-// const after = computed(() => {
-//   if (props.type === "action") return "\]"
-//   else if (props.type === "narrator") return "\}"
-//   else return ""
-// })
-
+  if (props.type === "action") return "\[";
+  else if (props.type === "narrator") return "\{";
+  else return "";
+});
 onMounted(() => {
   value.value = props.value;
-  console.log(input.value.attr)
+  console.log(input.value.attr);
 });
 </script>
 <style lang="css" scoped>
@@ -42,6 +58,7 @@ onMounted(() => {
   background-color: transparent;
   border: 1px solid transparent;
   padding: 1rem 2rem;
+  outline: 0;
 }
 
 .input:focus-visible {
@@ -52,13 +69,8 @@ onMounted(() => {
   margin-right: 5rem;
 }
 
-
-
-.action {
-  color: orange;
-}
-
-.narrator {
-  color: yellow;
+/* 按钮的种类 */
+.round:focus-visible {
+  border-radius: 1rem;
 }
 </style>
