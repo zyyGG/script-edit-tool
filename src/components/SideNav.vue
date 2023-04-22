5e39316ae784fd150a89b921845ad4730081dfab
Yanluoluo <1799191542@qq.com>
2023-04-22 14:51:19 +0800

    feat:新增侧边栏组件(可能弃用)

---
+++ src/components/SideNav.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <li class="col title">角色</li>
+    <!-- 内容展示区域 -->
+    <li class="col item action">A</li>
+    <li class="col item">B</li>
+    <li class="col item">C</li>
+    <li class="col item">D</li>
+    <li class="col add">Add</li>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="css" scoped>
+.container {
+  font-size: 1.5rem;
+  display: flex;
+  flex-direction: column; 
+  place-content: flex-start;
+  /* border-right: 2px solid #4e4e4e; */
+  margin-right: 0.3rem;
+  min-height: 100vh;
+  background-color: #282828;
+  text-align: center;
+}
+.title {
+  border-bottom: 0.1rem solid #eeeeee;
+}
+.col {
+  padding: 0.5rem 1.5rem;
+  cursor: default;
+}
+.item {
+  margin-block: 0.2rem;
+  cursor: pointer;
+}
+.action {
+  border-left: 0.2rem solid #5e5e5e;
+}
+</style>
