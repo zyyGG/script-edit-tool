04947e0e0d86215c2ad58fb9c95e2c19c5b23f62
Yanluoluo <1799191542@qq.com>
2023-04-22 14:49:52 +0800

    feat:新增总页面

---
+++ src/components/Description.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="grid title">
+      <span>人物</span>
+      <span>行为</span>
+      <span>场景</span>
+      <span>时间</span>
+    </div>
+
+    <div class="grid" v-for="item in store.state.date">
+      <span>{{ item.role }}</span>
+      <span>{{ item.behavior }}</span>
+      <span>{{ item.stage }}</span>
+      <span>{{ item.time }}</span>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { useStore } from "../store";
+const store = useStore();
+</script>
+<style lang="css" scoped>
+.container {
+  font-size: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  min-width: 100vw;
+}
+
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 5fr 1fr 1fr;
+  /* border: 1px solid #dddddd; */
+  /* border-top: 0; */
+}
+
+span {
+  display: block;
+  overflow-wrap: nowrap;
+  border: 1px solid rgba(255, 255, 255, 0.1);
+  padding: 0.5rem 1.5rem;
+}
+span:not(:nth-of-type(2)) {
+  text-align: center;
+}
+</style>
