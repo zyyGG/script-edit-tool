f2dbde74475f5085060d63f719b3e03a9f947343
Yanluoluo <1799191542@qq.com>
2023-04-22 14:51:03 +0800

    feat:新增更多的逻辑(组件可能弃用)

--- src/components/role.vue
+++ src/components/role.vue
@@ -1,13 +1,24 @@
+<!-- 页面顶部的角色选择区域 -->
 <template>
   <div class="container">
-    <!-- <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime expedita hic dolorem, dolore vitae, qui facere
-      placeat omnis eius distinctio iusto, quae officia quidem cumque. Inventore soluta voluptatibus aut. Neque.</p> -->
-    <Input type="text" value="Hello" />
+    <!-- 单个输入框 -->
+    <Input @handle-end-input="changeText" disabled />
+    <Button value="增加" />
   </div>
 </template>
 <script lang="ts" setup>
-import Input from './Input.vue';
+import { ref, reactive } from "vue";
+import Input from "./Input.vue";
+import Button from "./Button.vue";
 
+const text = ref("hello");
+
+const props = defineProps();
+const emits = defineEmits();
+
+function changeText(value: string) {
+  text.value = value;
+}
 </script>
 <style lang="css" scoped>
 .container {
