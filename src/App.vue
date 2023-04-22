1b20b29a751f87978bce1ce5c722e0c46faf5cf8
Yanluoluo <1799191542@qq.com>
2023-04-22 14:52:08 +0800

    feat:优化内容

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,16 @@
 <template>
-  <div class="container">
-    <Role />
-    <!-- <ChoosePerson /> -->
-    <!-- <Main /> -->
-    <!-- <Tool /> -->
-  </div>
+  <Description />
 </template>
 <script lang="ts" setup>
-import ChoosePerson from "./components/ChoosePerson.vue";
-import Main from "./components/Main.vue";
-import Tool from "./components/Tool.vue";
-import Role from "./components/Role.vue";
+import { onMounted } from "vue";
+import Description from "./components/Description.vue";
+// 测试
+import { test } from "./test";
 
+onMounted(() => {
+  test();
+});
 </script>
-<style lang="css">
-* {
-  margin: 0;
-}
-
-html {
-  font-size: 62.5%;
-  /* font-size: 70%; */
-}
-
-
+<style lang="css" scoped>
 /* 整体样式 */
-.container {
-  display: flex;
-  box-sizing: border-box;
-  place-content: flex-start;
-  place-items: flex-start;
-  flex-wrap: wrap;
-}
-
-/* 添加一个新的 */
-.addButton {
-  cursor: pointer;
-}
 </style>
